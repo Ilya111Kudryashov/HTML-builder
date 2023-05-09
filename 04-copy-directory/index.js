2d9fbceaade2dcfdb421d5d0514880705d0b704e
Ilya_Kudryashov <Zikbor@yandex.ru>
2023-05-09 03:31:55 +0300

    init: add task 4 copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs=require('fs');
+const path=require('path');
+const promises=fs.promises;
+const copyFile=promises.copyFile;
 
+(function copyDir(){
+    fs.mkdir(path.join(__dirname,'files-copy'),{
+        recursive:true,
+    }, err=>{
+        if(err){
+            throw new Error('folder already exsists or it maybe anither problem');
+        }
+        console.log('folder created');
+    });
+
+    
+    promises
+        .readdir(path.join(__dirname,'files'))
+        .then(files=>{
+            files.forEach(file => {
+                const filePath=path.join(__dirname,'files',file);
+                copyFile(filePath,path.join(__dirname,'files-copy',file));
+                console.log(file);
+            });
+        });
+})();
